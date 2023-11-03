653a23d86fe1a8bb24efb60f51b62de7ed65a60e
Pranay <66739544+PranayB003@users.noreply.github.com>
2023-11-03 11:01:01 +0530

    refactored: regfile

--- src/regfile.v
+++ src/regfile.v
@@ -1,28 +1,26 @@
 `include "constants.vh"
 
-module reg_file (clk, w_en, rd_addr, rs1_addr, rs2_addr, rd_val, rs1_val, rs2_val);
+module reg_file (clk, w_en, ra1, ra2, wa3, rd1, rd2, wd3);
 
   parameter word_width = `WORD_WIDTH;
   parameter addr_width = `REG_ADDR_WIDTH;
   parameter nregs      = 1 << addr_width;
 
   input wire clk, w_en;
-  input [addr_width-1:0] rd_addr, rs1_addr, rs2_addr;
-  input [word_width-1:0] rd_val;
-  output [word_width-1:0] rs1_val, rs2_val;
+  input [addr_width-1:0] ra1, ra2, wa3;
+  input [word_width-1:0] wd3;
+  output [word_width-1:0] rd1, rd2;
 
-  integer i;
   reg [word_width-1:0] regs [nregs-1:0];
 
   always @(posedge clk)
   begin
-    if (w_en && rd_addr != {addr_width{1'b0}}) begin
-      regs[rd_addr] <= rd_val; // synchronous write
+    if (w_en && wa3 != 0)
+    begin
+      regs[wa3] <= wd3; // synchronous write before read
     end
+    rd1 <= (ra1 != 0) ? regs[ra1] : 0;
+    rd2 <= (ra2 != 0) ? regs[ra2] : 0;
   end
-
-  // asynchronous read
-  assign rs1_val = (rs1_addr == {addr_width{1'b0}}) ? {word_width{1'b0}} : regs[rs1_addr];
-  assign rs2_val = (rs2_addr == {addr_width{1'b0}}) ? {word_width{1'b0}} : regs[rs2_addr];
 
 endmodule
