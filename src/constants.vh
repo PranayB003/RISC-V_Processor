95b8e5ff288934a88af4658cd7deddeac506ee7c
Pranay <66739544+PranayB003@users.noreply.github.com>
2023-11-03 10:30:31 +0530

    modified: constants and parameters

--- src/constants.vh
+++ src/constants.vh
@@ -1,5 +1,4 @@
-`define WORDSZ    32
-`define NREGS     32
-`define REGADDRSZ 5
-`define IMEMSZ    1024
-`define DMEMSZ    1024
+`define WORD_WIDTH     32
+`define REG_ADDR_WIDTH 5
+`define IMEM_DEPTH     1024
+`define DMEM_DEPTH     1024
--- src/regfile.v
+++ src/regfile.v
@@ -1,23 +1,28 @@
 `include "constants.vh"
 
-module reg_file (clk, reset, w_en, rd_addr, rs1_addr, rs2_addr, rd_val, rs1_val, rs2_val);
+module reg_file (clk, w_en, rd_addr, rs1_addr, rs2_addr, rd_val, rs1_val, rs2_val);
 
-  input wire clk, reset, w_en;
-  input [`REGADDRSZ-1:0] rd_addr, rs1_addr, rs2_addr;
-  input [`WORDSZ-1:0] rd_val;
-  output [`WORDSZ-1:0] rs1_val, rs2_val;
+  parameter word_width = `WORD_WIDTH;
+  parameter addr_width = `REG_ADDR_WIDTH;
+  parameter nregs      = 1 << addr_width;
+
+  input wire clk, w_en;
+  input [addr_width-1:0] rd_addr, rs1_addr, rs2_addr;
+  input [word_width-1:0] rd_val;
+  output [word_width-1:0] rs1_val, rs2_val;
 
   integer i;
-  reg [`WORDSZ-1:0]regs[`NREGS-1:0];
+  reg [word_width-1:0] regs [nregs-1:0];
 
   always @(posedge clk)
   begin
-    if (w_en && rd_addr != {`REGADDRSZ{1'b0}}) begin
-      regs[rd_addr] <= rd_val;
+    if (w_en && rd_addr != {addr_width{1'b0}}) begin
+      regs[rd_addr] <= rd_val; // synchronous write
     end
   end
 
-  assign rs1_val = (rs1_addr == {`REGADDRSZ{1'b0}}) ? {`WORDSZ{1'b0}} : regs[rs1_addr];
-  assign rs2_val = (rs2_addr == {`REGADDRSZ{1'b0}}) ? {`WORDSZ{1'b0}} : regs[rs2_addr];
+  // asynchronous read
+  assign rs1_val = (rs1_addr == {addr_width{1'b0}}) ? {word_width{1'b0}} : regs[rs1_addr];
+  assign rs2_val = (rs2_addr == {addr_width{1'b0}}) ? {word_width{1'b0}} : regs[rs2_addr];
 
 endmodule
