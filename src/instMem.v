185b6b1f81e9a471ed3a34048ed9a70df981b462
Pranay <66739544+PranayB003@users.noreply.github.com>
2023-11-04 16:12:34 +0530

    added: opcode and funct3 constants

--- src/instMem.v
+++ src/instMem.v
@@ -4,7 +4,7 @@ module instMemory (addr, inst);
 
   parameter addr_width = `MEM_ADDR_WIDTH;
   parameter mem_size   = `IMEM_DEPTH;
-  parameter word_width = `WORD_WIDTH
+  parameter word_width = `WORD_WIDTH;
   
   input wire [addr_width-1:0] addr;
   output wire [word_width-1:0] inst;
@@ -15,7 +15,7 @@ module instMemory (addr, inst);
 
   initial
   begin
-    $readmemb("int_mem.bin", memory);
+    $readmemb("inst_mem.bin", memory);
   end
 
 endmodule
