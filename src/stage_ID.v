131077db56607ca60cc4281308e9972c2d98ba52
Pranay <66739544+PranayB003@users.noreply.github.com>
2023-11-11 10:51:21 +0530

    fixed: pipeline registers in ID stage

--- src/stage_ID.v
+++ src/stage_ID.v
@@ -1,6 +1,6 @@
 `include "constants.vh"
 
-module stage_ID (clk, inst, pc_addr_in, rd_addr_in, rd_wen_in, wb_out, 
+module stage_ID (clk, inst_in, pc_addr_in, rd_addr_in, rd_wen_in, wb_out, 
                  rs1, rs2, rs1_val, rs2_val, rd_addr_out,
                  mux_ctl, alu_ctl, jmp_ctl, rd_wen_out, wb_ctl, bch_ctl, mem_ctl, byt_typ,
                  imm_ext, pc_addr_out, stall_en, pc_en);
@@ -10,7 +10,7 @@ module stage_ID (clk, inst, pc_addr_in, rd_addr_in, rd_wen_in, wb_out,
   parameter word_width     = `WORD_WIDTH;
 
   input wire clk, rd_wen_in;
-  input wire [word_width-1:0] inst, wb_out;
+  input wire [word_width-1:0] inst_in, wb_out;
   input wire [ins_addr_width-1:0] pc_addr_in;
   input wire [reg_addr_width-1:0] rd_addr_in;
   output reg [ins_addr_width-1:0] pc_addr_out;
@@ -21,16 +21,27 @@ module stage_ID (clk, inst, pc_addr_in, rd_addr_in, rd_wen_in, wb_out,
   output wire [1:0] wb_ctl;
   output wire jmp_ctl, rd_wen_out, bch_ctl, mem_ctl, stall_en, pc_en;
 
+  // Pipeline registers
+  reg [word_width-1:0] inst;
+  reg [ins_addr_width-1:0] pc_addr;
+
+  // Internal wires
   wire [6:0] opc    = inst[6:0], funct7 = inst[31:25];
   wire [4:0] ra1    = inst[19:15], ra2 = inst[24:20];
   wire [2:0] funct3 = inst[14:12];
+
+  always @(posedge clk)
+  begin
+    inst    <= inst_in;
+    pc_addr <= pc_addr_in;
+  end
 
   always @(*)
   begin
     rs1         = ra1;
     rs2         = ra2;
     rd_addr_out = inst[11:7];
-    pc_addr_out = pc_addr_in;
+    pc_addr_out = pc_addr;
   end
 
   regfile id_reg(
